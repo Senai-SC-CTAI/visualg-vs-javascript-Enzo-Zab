9ec693f8a6b9f8c210465ae3505f798076bc0d63
Enzo Zabadal <125047212+Enzo-Zab@users.noreply.github.com>
2023-08-03 11:53:05 -0300

    Projeto algoritmo "bombons"

---
+++ bombons.js
@@ -0,0 +1,10 @@
+
+function botão1(){
+    
+    var alunos = document.getElementById("alunos").value
+    var bombons = alunos * 2 + 1
+    
+    
+document.getElementById("resposta").innerHTML ="A escola terá que comprar "+bombons+" bombons para a turma"
+
+}
